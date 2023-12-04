3cca18f2d5607d69f55abd8b0a8a7c776bc0b90f
XiangXisheng <xiangxisheng@gmail.com>
2023-12-04 16:59:58 +0800

    1

--- wwwroot/page/panel.js
+++ wwwroot/page/panel.js
@@ -2,13 +2,16 @@ export default {
   template: await (await fetch('./page/panel.htm')).text(),
   data() {
     return {
-
+      sidebar_collapse: false
     }
   },
   async created() {
 
   },
   methods: {
-
+    sidebar_collapse_switch() {
+      return;
+      this.sidebar_collapse = !this.sidebar_collapse;
+    }
   },
 }
